90484ad5799264eae8e33692856c568b50c5aafd
Ma Guadalupe Brizuela Gamiño <brizuelautl@gmail.com>
2020-05-01 17:12:52 -0500

    Carga inicial

---
+++ micro-servicio/environment/environment.production.ts
@@ -0,0 +1,21 @@
+export default {
+    API: {
+        NAME: 'Micro-Servicio Punto de Venta | NodeJS',
+        PORT: 5000,
+        ENVIRONMENT: 'Development'
+    },
+    NOTIFY: {
+        DELAY: 1000 * 10        // 10 Segundos
+    },
+    TOKEN: {
+        SECRET_KEY: 'secretkey-value',
+        EXPIRES: '120s'    // 2 Minutos
+    },
+    MONGODB: {
+        HOST: '192.168.252.132',
+        PORT: 27017,
+        USER_NAME: 'lupita',
+        USER_PASSWORD: '1234',
+        DEFAULT_DATABASE: 'admin'
+    }
+};
